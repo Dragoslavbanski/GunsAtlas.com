83bac95b29bdc339175bd490566ccdc474351839
Dragoslav Banski <dz_banski@abv.bg>
2026-07-18 23:53:38 +0300

    Initial GunsAtlas version

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header>
+      <h2>GunsAtlas</h2>
+
+      <nav>
+        <a href="#">Начало</a>
+        <a href="#">Пистолети</a>
+        <a href="#">Револвери</a>
+        <a href="#">Боеприпаси</a>
+        <a href="#">За проекта</a>
+      </nav>
+    </header>
+  );
+}
